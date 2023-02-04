a091c507c955e8b3cdd05dcc7dd8835df659a516
Bradley Griffin <bigriff11@gmail.com>
2023-02-04 11:14:29 -0700

    Update nav items

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,20 +1,29 @@
-import { FaBeer } from 'react-icons/fa';
+import { SiInstagram, SiTiktok, SiYoutube, SiApple, SiSpotify, SiPandora } from 'react-icons/si';
 
 let Nav = () => {
     return (
-      <nav className="sticky top-0 z-10 bg-black p-3 flex justify-between items-center bg-opacity-10 backdrop-blur-md">
-        <a href="#home" className="text-white font-bold">
-          My Navbar
+      <nav className="sticky top-0 z-10 bg-black p-3 flex justify-between items-center bg-opacity-20 backdrop-blur-md">
+        <a href="#home" className="text-white font-bold text-2xl">
+          Sarah States
         </a>
         <div className="flex items-center">
           <a href="#home" className="text-white mx-3 bg-white p-2 rounded-md bg-opacity-20">
-            <FaBeer />
+            <SiInstagram />
           </a>
-          <a href="#home" className="text-white mr-3">
-            About
+          <a href="#home" className="text-white mx-3 bg-white p-2 rounded-md bg-opacity-20">
+            <SiTiktok />
           </a>
-          <a href="#home" className="text-white">
-            Contact
+          <a href="#home" className="text-white mx-3 bg-white p-2 rounded-md bg-opacity-20">
+            <SiYoutube />
+          </a>
+          <a href="#home" className="text-white mx-3 bg-white p-2 rounded-md bg-opacity-20">
+            <SiApple />
+          </a>
+          <a href="#home" className="text-white mx-3 bg-white p-2 rounded-md bg-opacity-20">
+            <SiPandora />
+          </a>
+          <a href="#home" className="text-white mx-3 bg-white p-2 rounded-md bg-opacity-20">
+            <SiSpotify />
           </a>
         </div>
       </nav>
