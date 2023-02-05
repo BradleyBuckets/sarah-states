6198ce50718feb0c0dfbef980676a9a254ab780c
Bradley Griffin <bigriff11@gmail.com>
2023-02-04 17:44:02 -0700

    Add items for email capture

--- src/components/Email.jsx
+++ src/components/Email.jsx
@@ -8,10 +8,20 @@ let Email = () => {
         <h2 className=" text-neutral-50 font-extrabold text-4xl drop-shadow-lg">
             Newsletter
         </h2>
-        <p className="text-neutral-50 font-bold drop-shadow-lg w-80 text-center">
+        <p className="text-neutral-50 font-bold drop-shadow-lg w-72 text-center">
             Sed quis nibh cras pulvinar mattis nunc libero amet
         </p>
-        
+        <div className=" bg-gradient-to-br from-primary-gray-dark to-primary-gray-light w-80 h-60 rounded-lg">
+            <div className=" w-64 h-10 text-neutral-50 font-bold bg-neutral-50 bg-opacity-20 rounded-lg flex items-center">
+                Name
+            </div>
+            <div className=" w-40 h-10 text-neutral-50 font-bold bg-neutral-50 bg-opacity-20 rounded-lg flex items-center">
+                Email
+            </div>
+            <div className=" w-44 h-10 text-neutral-50 font-bold bg-gradient-to-br to-primary-purple from-secondary-pink rounded-lg flex justify-center items-center">
+                Subscribe
+            </div>
+        </div>
      </div>
     );
   };
