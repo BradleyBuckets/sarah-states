c26da4856143e360be5324acc8629e512601fbad
Bradley Griffin <bigriff11@gmail.com>
2023-02-08 12:20:12 -0700

    Add drop shadow to support

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -5,10 +5,10 @@ import { BsChevronRight } from 'react-icons/bs'
 let Support = () => {
     return (
      <div className="w-screen bg-gradient-to-tr from-primary-purple to-primary-pink flex flex-col items-center justify-center gap-10 py-20">
-        <div className=" text-neutral-50 flex justify-center items-center text-8xl m-2">
+        <div className=" text-neutral-50 flex justify-center items-center text-8xl m-2 drop-shadow-xl">
             Support my music!
         </div>
-        <div className=" w-36 h-12 flex justify-center items-center text-neutral-50 rounded-xl bg-neutral-400 border border-neutral-50 hover:bg-opacity-80 hover:cursor-pointer m-2 gap-2">
+        <div className=" w-36 h-12 flex justify-center items-center text-neutral-50 rounded-xl bg-neutral-400 border border-neutral-50 hover:bg-opacity-80 hover:cursor-pointer m-2 gap-2 drop-shadow-xl">
             <div>Purchase</div>
             <div>
                 <BsChevronRight />
