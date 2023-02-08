7ed63c9230e669aaf49230a848fdb82fa09e7867
Bradley Griffin <bigriff11@gmail.com>
2023-02-08 14:19:49 -0700

    Remove all uneeded items in app

--- src/App.js
+++ src/App.js
@@ -9,25 +9,8 @@ import MusicCard2 from "./components/MusicCard2";
 
 function App() {
   return (
-    <div className="App bg-slate-400 ">
+    <div className="App">
       <Nav />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <h1 className="text-3xl font-bold text-cyan-500 underline">
-        Hello world!
-      </h1>
       <Hero />
       <MusicCard />
       <MusicCard2 />
