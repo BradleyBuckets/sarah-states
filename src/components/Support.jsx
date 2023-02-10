268462c0b54cad1ae944e83ca723d1e8cd44f919
Bradley Griffin <bigriff11@gmail.com>
2023-02-10 14:52:45 -0700

    Make support mobile responsive

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -5,7 +5,7 @@ import { BsChevronRight } from 'react-icons/bs'
 let Support = () => {
     return (
      <div className="w-screen bg-gradient-to-tr from-primary-purple to-primary-pink flex flex-col items-center justify-center gap-10 py-20">
-        <div className=" text-neutral-50 flex justify-center items-center text-8xl m-2 drop-shadow-xl">
+        <div className=" text-neutral-50 flex justify-center items-center text-6xl sm:text-7xl md:text-8xl m-2 drop-shadow-xl text-center">
             Support my music!
         </div>
         <div className=" w-36 h-12 flex justify-center items-center text-neutral-50 bg-neutral-50 p-2 rounded-lg bg-opacity-10 gap-2 drop-shadow-xl border border-neutral-50 hover:bg-opacity-20 hover:cursor-pointer">
