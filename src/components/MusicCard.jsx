0e145c31ee8caf7adefcbcbc02facf8494b9b9ae
Bradley Griffin <bigriff11@gmail.com>
2023-02-10 14:46:58 -0700

    make music card mobile responsive

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -8,23 +8,23 @@ let MusicCard = () => {
         <div className="bg-gradient-to-br from-primary-gray-dark to-primary-gray-light w-11/12 sm:w-10/12 md:w-8/12 rounded-xl p-4 sm:p-8 flex flex-col lg:flex-row drop-shadow-xl gap-4">
             <img src={img} alt="A pretty girl smiling" className="lg:w-1/2 rounded-xl flex-[3_3_0%] object-cover"/>
             <div className="flex flex-col lg:px-2 flex-[2_2_0%]">
-                <div className="sm:m-1">
+                <div className="sm-1">
                     <p className="text-neutral-50 opacity-70 text-sm mx-4 my-1">Song title here</p>
                     <audio src={audio} controls className="w-full opacity-50"></audio>
                 </div>
-                <div className="m-1">
+                <div className="sm-1">
                     <p className="text-neutral-50 opacity-70 text-sm mx-4 my-1">Song title here</p>
                     <audio src={audio} controls className="w-full opacity-50"></audio>
                 </div>
-                <div className="m-1">
+                <div className="sm-1">
                     <p className="text-neutral-50 opacity-70 text-sm mx-4 my-1">Song title here</p>
                     <audio src={audio} controls className="w-full opacity-50"></audio>
                 </div>
-                <div className="m-1">
+                <div className="sm-1">
                     <p className="text-neutral-50 opacity-70 text-sm mx-4 my-1">Song title here</p>
                     <audio src={audio} controls className="w-full opacity-50"></audio>
                 </div>
-                <div className="m-1">
+                <div className="sm-1">
                     <p className="text-neutral-50 opacity-70 text-sm mx-4 my-1">Song title here</p>
                     <audio src={audio} controls className="w-full opacity-50"></audio>
                 </div>
--- src/components/MusicCard2.jsx
+++ src/components/MusicCard2.jsx
@@ -5,12 +5,12 @@ import audio from "../resources/music.mp3"
 let MusicCard2 = () => {
     return (
      <div className=" h-screen w-screen bg-gradient-to-br from-primary-purple to-primary-pink flex flex-col items-center justify-center gap-10 pb-10">
-        <div className="bg-gradient-to-br from-primary-gray-dark to-primary-gray-light w-8/12 rounded-xl p-8 flex drop-shadow-xl gap-4">
-            <img src={img} alt="A pretty girl smiling" className="w-1/2 rounded-xl flex-[3_3_0%]"/>
-            <div className="flex flex-col px-2 flex-[2_2_0%]">
-                <div className="m-1">
+        <div className="bg-gradient-to-br from-primary-gray-dark to-primary-gray-light w-11/12 sm:w-10/12 md:w-8/12 rounded-xl p-4 sm:p-8 flex flex-col lg:flex-row drop-shadow-xl gap-4">
+            <img src={img} alt="A pretty girl smiling" className="lg:w-1/2 rounded-xl flex-[3_3_0%] object-cover"/>
+            <div className="flex flex-col lg:px-2 flex-[2_2_0%]">
+                <div className="sm-1">
                     <p className="text-neutral-50 opacity-70 text-sm mx-4 my-1">Song title here</p>
-                    <audio src={audio} controls className=" opacity-50"></audio>
+                    <audio src={audio} controls className="w-full opacity-50"></audio>
                 </div>
             </div>
         </div>
