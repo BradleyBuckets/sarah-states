2223401321c47b4061e3f78da7993be533f27532
Bradley Griffin <bigriff11@gmail.com>
2023-02-12 15:48:32 -0700

    Add trasition duration to button

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -8,7 +8,7 @@ let Support = () => {
         <div className=" text-neutral-50 flex justify-center items-center text-6xl sm:text-7xl md:text-8xl m-2 drop-shadow-xl text-center">
             Support my music!
         </div>
-        <div className=" w-36 h-12 flex justify-center items-center text-neutral-50 bg-neutral-50 p-2 rounded-lg bg-opacity-10 gap-2 drop-shadow-xl border border-neutral-50 hover:bg-opacity-20 hover:cursor-pointer">
+        <div className=" w-36 h-12 flex justify-center items-center text-neutral-50 bg-neutral-50 p-2 rounded-lg bg-opacity-10 gap-2 drop-shadow-xl border border-neutral-50 hover:bg-opacity-20 hover:cursor-pointer ease-in-out duration-200">
             <div>Purchase</div>
             <div>
                 <BsChevronRight />
