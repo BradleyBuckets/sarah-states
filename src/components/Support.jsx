de2ebe4f67bec61e8637f66a6132dac113cd9076
Bradley Griffin <bigriff11@gmail.com>
2023-04-07 19:56:16 -0600

    Add link to open in new tab and change to donate

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -8,8 +8,9 @@ let Support = () => {
         <div className=" text-neutral-50 flex justify-center items-center text-6xl sm:text-7xl md:text-8xl m-2 drop-shadow-xl text-center">
             Support my music!
         </div>
-        <a href="https://venmo.com/u/sarahhh-states" className=" w-36 h-12 flex justify-center items-center text-neutral-50 bg-neutral-50 p-2 rounded-lg bg-opacity-10 gap-2 drop-shadow-xl border border-neutral-50 hover:bg-opacity-20 hover:cursor-pointer ease-in-out duration-200">
-            <div>Purchase</div>
+        <a href="https://venmo.com/u/sarahhh-states" target="_blank" className=" w-36 h-12 flex justify-center items-center text-neutral-50 bg-neutral-50 p-2 rounded-lg bg-opacity-10 gap-2 drop-shadow-xl border border-neutral-50 hover:bg-opacity-20 hover:cursor-pointer ease-in-out duration-200" rel="noreferrer">
+            {/* <div>Purchase</div> */}
+            <div>Donate</div>
             <div>
                 <BsChevronRight />
             </div>
